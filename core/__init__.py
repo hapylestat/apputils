008ce0c1c7402bd66af80b9cf5712bb17b3fe443
Reishin <hapy.lestat@gmail.com>
2016-03-07 09:29:23 +0200

    Update according to new module structuru

--- core/__init__.py
+++ core/__init__.py
@@ -1,30 +1,7 @@
 
-from appcore.core.config import Configuration
+from appcore.core.singlobj import Singleton, SingletonObject
 from appcore.core.logger import aLogger
-
-
-class Singleton(object):
-
-  def __init__(self, decorated):
-    self._decorated = decorated
-
-  def get_instance(self):
-    if '_instance' in dir(self):
-      return self._instance
-    else:
-      self._instance = self._decorated()
-      return self._instance
-
-  def __call__(self):
-    raise TypeError('Singletons must be accessed through `get_instance()`.')
-
-  def __instancecheck__(self, inst):
-    return isinstance(inst, self._decorated)
-
-
-class SingletonObject(object):
-  def get_instance(self):
-    return self
+from appcore.core.config import Configuration
 
 
 __all__ = ["Configuration", "aLogger", "Singleton", "SingletonObject"]
