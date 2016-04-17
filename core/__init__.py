c4d973b2049898548ad016f00a0e593540850fc0
Reishin <hapy.lestat@gmail.com>
2016-04-17 16:20:18 +0300

    Fixes and Cleanups
    Configuration:
    - add default parameter
    - add script name property
    - lgpl license

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,5 @@
-
 from appcore.core.singlobj import Singleton, SingletonObject
 from appcore.core.logger import aLogger
 from appcore.core.config import Configuration
-
 
 __all__ = ["Configuration", "aLogger", "Singleton", "SingletonObject"]
