75aaa987114fa606064c437092d284048b948d3f
dgrinenko <dgrinenko@hortonworks.com>
2016-04-26 19:02:24 +0300

    Introduce setup script

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-from appcore.core.singlobj import Singleton, SingletonObject
-from appcore.core.logger import aLogger
-from appcore.core.config import Configuration
-
-__all__ = ["Configuration", "aLogger", "Singleton", "SingletonObject"]
