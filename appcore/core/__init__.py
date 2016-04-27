b0aad45252c914001f460fadddd67f79b3a72f33
Reishin <hapy.lestat@gmail.com>
2016-04-27 15:22:23 +0300

    Add possibility to serialize

--- appcore/core/__init__.py
+++ appcore/core/__init__.py
@@ -1,10 +1,17 @@
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# Copyright (c) 2016 Reishin <hapy.lestat@gmail.com>
+
 from appcore.core.singlobj import Singleton, SingletonObject
 from appcore.core.logger import aLogger
 from appcore.core.config import Configuration
 from appcore.core import config
 
 __name__ = "appcore"
-__version__ = "0.1.1"
+__version__ = "0.1.2.1"
 __author__ = "Dmytro Grinenko"
 __author_mail__ = "hapy.lestat@gmail.com"
 __url__ = "https://github.com/hapylestat/appcore"
